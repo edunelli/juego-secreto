34a2b0bbe2ad6c9de708796e36f42218fd597fd3
edunelli <nelliedu67@gmail.com>
2025-09-01 15:59:51 -0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log("ejecutando el sistema")
+// inicio del proyecto
+let valor = "";
+let amigosAgregados = [];
+
+// funcion para agregar amigos
+
+function agregarAmigo(nombre){
+    let inputAmigo = document.getElementById('amigo');
+    if (inputAmigo.value == "") {
+        alert("Debe ingresar un nombre");
+    } else {
+        valor = inputAmigo.value;
+        amigosAgregados.push(valor);
+        console.log(amigosAgregados);
+        agregarLista(amigosAgregados);
+        document.getElementById('amigo').value = "";
+    }
+    return amigosAgregados;
+}
+
+// funcion para agregar la lista de amigos al html
+
+function agregarLista(lista) {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i < lista.length; i++) {
+        listaAmigos.innerHTML += `<li>${lista[i]}</li>`;
+    }
+}
+
+// funcion para agregar amigos
+
+function sortearAmigo() {
+    let numeroAleatorio = 0;
+    if (amigosAgregados.length != 0){
+        numeroAleatorio = Math.floor(Math.random()* amigosAgregados.length);// genera numero aleatorio, en base a el largo de la lista
+        let resultado = document.getElementById('resultado')
+        resultado.innerHTML = `<li>${amigosAgregados[numeroAleatorio]}</li>`;// crea el elemento lista con el amigo sorteado
+    } else {
+        alert("Debe ingresar al menos un amigo");
+    }
+}
